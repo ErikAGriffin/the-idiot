aad7dbf7c5d5b6161d490d637a0a9d8dad3876ce
Erik Griffin <griffin.erik@gmail.com>
2015-02-23 12:15:12 +0000

    Basic deck

---
+++ lib/deck.rb
@@ -0,0 +1,17 @@
+class Deck < Array
+
+
+  def draw
+    self.pop
+  end
+
+  def shuffle
+    self.shuffle!
+  end
+
+  def top_card
+    self.last
+  end
+
+
+end
---
+++ spec/deck_spec.rb
@@ -0,0 +1,28 @@
+require 'deck'
+
+describe 'Deck' do
+
+  let(:card) {double :card}
+
+  let(:deck) {Deck.new(52, card)}
+
+
+  it 'is an array that can hold cards' do
+    expect(deck.count).to eq 52
+  end
+
+  it 'can have the top card taken off' do
+    expect(deck.draw).to eq card
+    expect(deck.count).to eq 51
+  end
+
+  it 'can shuffle itself' do
+    deck.draw
+    king = double :card
+    deck << king
+    deck.shuffle
+    expect(deck.top_card).to_not eq king
+  end
+
+
+end
