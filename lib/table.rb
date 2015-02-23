b055fbd0c609ceceefbf4d3c9600c8442ca88a4c
Erik Griffin <griffin.erik@gmail.com>
2015-02-23 14:14:49 +0000

    basic table

--- lib/table.rb
+++ lib/table.rb
@@ -1,4 +1,19 @@
-class Table
+class Table < Array
+
+  def place(card)
+    self << card
+  end
+
+  def burn!
+    self.clear
+  end
+
+  def topcard
+    self.last
+  end
+
+
+
 
 
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -2,9 +2,30 @@ require 'table'
 
 describe 'Table' do
 
-  it 'exists' do
+  let(:card) {double :card}
+  let(:table) {Table.new}
 
+  it 'is an array that holds cards' do
+    expect(table).to eq []
   end
+
+  it 'can have a card placed on the table' do
+    table.place(card)
+    expect(table).to eq [card]
+  end
+
+  it 'can burn itself, removing those cards from the game' do
+    table.place(card)
+    table.burn!
+    expect(table).to eq []
+  end
+
+  it 'shows the topcard' do
+    table.place(card)
+    expect(table.topcard).to eq card
+  end
+
+
 
 
 end
