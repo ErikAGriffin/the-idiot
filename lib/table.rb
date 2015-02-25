1f262607c0062e90d558a3eceb11990c1dcc0c8c
Erik Griffin <griffin.erik@gmail.com>
2015-02-25 18:51:09 +0000

    Refactoring how table handles its 'rule'

--- lib/table.rb
+++ lib/table.rb
@@ -1,4 +1,21 @@
+class TableRule
+
+  # How can I make this more protected,
+  # yet allow the table and the table alone
+  # to always modify these properties
+
+  attr_accessor :rank, :rule
+  def initialize
+    @rule = 4
+  end
+end
+
 class Table < Array
+
+  def initialize
+    super
+    @rule = TableRule.new
+  end
 
   def place(card)
     self << card
@@ -9,7 +26,14 @@ class Table < Array
   end
 
   def topcard
-    self.last
+    if self.last
+      @rule.rank = self.last.rank
+      @rule.rule = self.last.rank
+    else
+      @rule.rank = 0
+      @rule.rule = 4
+    end
+    @rule
   end
 
 
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -2,7 +2,7 @@ require 'table'
 
 describe 'Table' do
 
-  let(:card) {double :card}
+  let(:card) {double :card, rank: 4}
   let(:table) {Table.new}
 
   it 'is an array that holds cards' do
@@ -20,14 +20,32 @@ describe 'Table' do
     expect(table).to eq []
   end
 
-  it 'shows the topcard' do
+  it "displays the current table 'rule'" do
     table.place(card)
-    expect(table.topcard).to eq card
+    expect(table.topcard.rule).to eq card.rank
   end
 
   context 'Special Cards' do
 
+    it 'zeros out the table if a 2 is played' do
+      two_spades = double :card, rank: 2
+      table.place(two_spades)
+      expect(table.topcard.rank <= 4).to eq true
+    end
 
+    # !! ** !!
+    # Ahh... I think I'm going to have to make an object for
+    # what I call the 'table rule', aka what the game is presenting
+    # for you to beat.
+    # Because of the condition that a 3 can only jump a 3, but the value is
+    # whatever is underneath
+
+    it 'passes on present value if a 3 is played' do
+      queen_hearts = double :card, rank: 12
+      three_spades = double :card, rank: 3
+      table.place(queen_hearts)
+      table.place(three_spades)
+    end
 
   end
 
