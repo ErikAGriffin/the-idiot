5378e808409a6b032384c6694cc1b724b5d35be1
Erik Griffin <griffin.erik@gmail.com>
2015-02-26 12:25:59 +0000

    Final commit of Pre-Web model. Not 100% complete, but functional

--- GameMaster.rb
+++ GameMaster.rb
@@ -1,25 +1,5 @@
 require './r'
 
-def build_deck(deck)
-  suits = [:♠,:♥,:♦,:♣]
-  ranks = [*2..14]
-
-  ranks.each do |rank|
-    suits.each do |suit|
-      deck << Card.new(rank,suit)
-    end
-  end
-  deck.shuffle
-end
-
-def deal_to_players_in(game,deck)
-  9.times do
-    game.players.each do |player|
-      player.deal(deck.draw)
-    end
-  end
-end
-
 
 deck = Deck.new
 table = Table.new
--- r.rb
+++ r.rb
@@ -3,3 +3,26 @@ require './lib/card'
 require './lib/table'
 require './lib/player'
 require './lib/idiot_game_logic'
+
+
+
+
+def build_deck(deck)
+  suits = [:♠,:♥,:♦,:♣]
+  ranks = [*2..14]
+
+  ranks.each do |rank|
+    suits.each do |suit|
+      deck.add_card(Card.new(rank,suit))
+    end
+  end
+  deck.shuffle
+end
+
+def deal_to_players_in(game,deck)
+  9.times do
+    game.players.each do |player|
+      player.deal(deck.draw)
+    end
+  end
+end
